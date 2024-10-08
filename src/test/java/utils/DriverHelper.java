532073ece55241333b9792542d963a4c316f1072
techtorialacademy <122166999+techtorialacademy@users.noreply.github.com>
2024-10-08 17:16:08 -0500

    Update DriverHelper.java

--- src/test/java/utils/DriverHelper.java
+++ src/test/java/utils/DriverHelper.java
@@ -30,7 +30,7 @@ public class DriverHelper {
             switch (ConfigReader.readProperty("browser")) {
 
                 case "chrome":
-                    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+                   
                     ChromeOptions options = new ChromeOptions();
                     options.addArguments("--headless");
                     options.addArguments("--no-sandbox");
